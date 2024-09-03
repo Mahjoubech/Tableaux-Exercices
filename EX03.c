f7fb99faca4d22ba51c2d217121d7a81b4a51b60
mimo <104110561+Mahjoubech@users.noreply.github.com>
2024-09-03 17:32:07 +0100

    Create EX03.c
    EX03

---
+++ EX03.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+
+int T[5];
+int S,i;
+printf("Saisie des elements : \n");
+for(i=0;i<5;i++){
+    
+    scanf("%d",&T[i]);
+}
+printf("Afficharge des elements : \n");
+for(i=0;i<5;i++){
+    printf("T[%d] = %d\n",i+1,T[i]);
+}
+S=0;
+for(i=0;i<5;i++){
+    S+=T[i];
+}
+printf("la Somme est : %d", S);
+
+return 0 ;
+}
