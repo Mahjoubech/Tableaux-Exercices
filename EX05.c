74a44e80a29e78523799329377b7248105e42cc0
mimo <104110561+Mahjoubech@users.noreply.github.com>
2024-09-03 19:58:46 +0100

    Create EX05.c
    EX05

---
+++ EX05.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+
+int T[5];
+int min,i;
+printf("Saisie des elements : \n");
+for(i=0;i<5;i++){
+    
+    scanf("%d",&T[i]);
+}
+printf("Afficharge des elements : \n");
+for(i=0;i<5;i++){
+    printf("T[%d] = %d\n",i+1,T[i]);
+}
+min = T[0];
+for(i=1;i<5;i++){
+    if(min > T[i]){
+    min = T[i];
+    }
+}
+printf("Maximun  est : %d", min);
+
+return 0 ;
+}
