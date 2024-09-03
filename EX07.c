98f3d07d601eaf3349a5a9bf6eff27f4258f382e
mimo <104110561+Mahjoubech@users.noreply.github.com>
2024-09-03 22:58:37 +0100

    Create EX07.c
    EX07

---
+++ EX07.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main(){
+
+int n,i,j,cmp;
+printf("saisie index de tableau : ");
+scanf("%d",&n);
+int T[n];
+printf("Saisie des elements des tableaux : \n");
+for(i=0;i<n;i++){
+    
+    scanf("%d",&T[i]);
+}
+
+for(i=0;i<n;i++){
+   for(j=i+1;j<n;j++){
+   if(T[i]>T[j]){
+    cmp = T[i];
+    T[i] = T[j];
+    T[j] = cmp;
+   }
+   }
+}
+
+printf("Affichage de tableau tri croissant : \n");
+ for(i=0;i<n;i++){
+ printf(" %d ",T[i]);
+ }
+
+return 0 ;
+}
