4d83f23eab9c7b4e6a3d73d45e2c42b1071ae6c4
mimo <104110561+Mahjoubech@users.noreply.github.com>
2024-09-04 01:20:52 +0100

    Create EX13.c
    EX13

---
+++ EX13.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main() {
+
+    int n,i;
+    printf("saisie index de tableau : ");
+    scanf("%d",&n);
+    int T[n];
+    printf("Saisie des elements des tableaux : \n");
+    for(i=0; i<n; i++) {
+        printf("T[%d] = ",i+1);
+        scanf("%d",&T[i]);
+    }
+    printf("Affichage les elements impaires : \n");
+
+    for(i=0; i<n; i++) {
+        if(T[i]%2 != 0) {
+            printf("T[%d] = %d \n",i+1,T[i]);
+        }
+    }
+  
+
+    return 0 ;
+}
